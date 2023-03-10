c87b8c95b8ab8c8390497548655f3bac9e530097
kucsssss <117690564+kucsssss@users.noreply.github.com>
2023-03-10 21:51:11 +0100

    Update main.dart

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Person API Demo',
+      title: 'teszteszt',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),

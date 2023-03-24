b57fe573f5e406b6a396f4c44e6887d4d43d44c9
kucsssss <117690564+kucsssss@users.noreply.github.com>
2023-03-24 14:57:23 +0100

    Cross Origin error kijavitása
    local host után mehet a Flutter alkalmazás portja

--- userid/src/main/java/com/example/userid/api/PersonContoller.java
+++ userid/src/main/java/com/example/userid/api/PersonContoller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:")
 @RequestMapping("api/v1/person")
 @RestController
 public class PersonContoller {
